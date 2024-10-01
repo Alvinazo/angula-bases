41de93fd82fb829c8aecb24288880d81bafb5842
areyes <alvaroreyesc59@gmail.com>
2024-10-01 12:26:51 -0300

    Fin de Seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { CounterModule } from './counter/counter.module';
+import { heroesModule } from './heroes/heroes.module';
+
+
+import { AppComponent } from './app.component';
+
+
+//a-module sniper
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    heroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,56 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+public name:string= 'ironman';
+
+public age: number = 45;
+
+
+get capitalizedNamed():string{
+
+  return this.name.toUpperCase();
+}
+
+
+
+getHeroDescription():string{
+
+  return `${this.name} - ${this.age}`;
+ }
+
+
+changeHero():void{
+
+
+this.name = 'Spider Man';
+
+
+}
+
+
+changeAge():void{
+
+this.age = 25;
+
+
+}
+
+resetForm():void{
+
+this.name = 'IronMan';
+this.age = 45;
+
+}
+
+
+
+
+
+}
